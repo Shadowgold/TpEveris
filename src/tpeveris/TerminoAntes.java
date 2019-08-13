a8eca395111e7ada45f4b36ab548de70cf24fdfa
Fabrizio Pouget <f.pouget94@gmail.com>
2019-08-13 11:55:17 -0300

    Clase Extra

---
+++ src/tpeveris/TerminoAntes.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpeveris;
+
+/**
+ *
+ * @author fabri
+ */
+public class TerminoAntes {
+    
+    void testo(){
+        System.out.println("Mati Gato!!!");
+    }
+}
